4f86a73776dfecd4f38917806c3d3cd61f41947a
JoyRob <you@example.com>
2024-03-13 16:43:04 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta>
+  <img src="images/83A23CAF-8340-4ED8-81D9-4270837A3DDD.png" alt="logo" id="buisnesslogo">
+  <title>MY AKWAIBOM KITCHEN</title>
+  
+  <link rel="style" href="css/style.css">
+  <link rel="stylesheet" href="css/unsemantic-grid-responsive-tablet.css">
+  <link rel="stylesheet" href="css/footer.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+<body class="body">
+
+  <HEAder>
+
+      <nav class="">
+    
+        <ul class="navbar-nav">
+          <li class="nav-item"><a href="#" class="nav-link" >HOME</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">ABOUT</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">NEWS</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">SEARCH</a></li>
+          <li class="nav-item"><a href="login.php" class="nav-link">LOGIN/REGISTER</a></li>
+        </ul>
+      </nav>
+
+        <!-- The video -->
+        <video autoplay muted loop id="myVideo">
+          <source src="video/Video in Dark Green White Bold Style.mp4" type="video/mp4">
+        </video>
+
+        <!-- Optional: some overlay text to describe the video -->
+        <div class="content">
+          <h1>Heading</h1>
+          <p>Lorem ipsum...</p>
+          <!-- Use a button to pause/play the video with JavaScript -->
+          <button id="myBtn" onclick="myFunction()">Pause</button>
+        </div>
+
+    </div>
+  <div >
+      <img src="images/modern-matte-black-kitchen-cabinets-obk22-dark-1024.webp" alt="logo" id="mainimgage">
+    </div>
+    <h1 class="HEADER"> WELCOME TO IBOM FOODIE</h1>
+
+  
+  </HEAder>
+  <Main class="main">
+    <section>
+      <h2>Meet the CEO</h2>
+      <img src="images/IMG_1116.JPG" alt="ceo" id="joy">
+
+      <main>
+        <section>
+          <h1> SOME OF OUR FAVORITE RECIPES</h1>
+          <div class="container text-center">
+            <div class="row align-items-start">
+              <div class="col">
+                <h1> AFANG SOUP</h1>
+                <img src="images/afang.jpg" alt="chef Hilda" class="img-fluid ">
+                <p>Some cheese may be feta than this,but this one is still quite Gouda</p>
+              </div>
+              <div class="col">
+                <h1> JOLLOF RICE</h1>
+                <img src="images/jollof.jpeg" alt="cheft"class="img-fluid">
+                <p>This cheese is really good if you are looking to hide livestock</p>
+              </div>
+              <div class="col">
+                <h1>EDIKA EKONG</H1>
+                <img src="images/EDIKAEKONG.jpg" alt="Mountains"class="img-fluid">
+                <p>At the end the night all that is left is deBrie</p>
+              </div>
+        </section>
+      </main>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi neque ipsam soluta omnis, animi, voluptate sunt cumque perferendis nesciunt rem, corporis dolorem nemo in maxime facilis cupiditate ullam? Tempora, amet?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur reprehenderit libero velit quibusdam minus, dolorem laborum inventore ea distinctio eos amet eum sunt dolore saepe quae in sed a exercitationem.</p>
+      <h3>This is where the magic happens</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus aliquam odit, corrupti aperiam impedit obcaecati voluptatem quia beatae, unde sapiente quo nobis numquam blanditiis modi quisquam deleniti cum! Quas, aliquid.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quisquam harum, laudantium officia, necessitatibus unde facilis nisi assumenda, distinctio debitis minima tenetur corrupti minus voluptatibus commodi velit officiis neque. Incidunt?</p>
+      <h3>Create your own recipes from our list of comprehensive ingredients</h3>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis ullam in ipsam culpa dolorum ea! Nobis, dolorem. Error deleniti repellendus provident ipsam, recusandae doloribus exercitationem voluptas eum distinctio omnis cumque?</p>
+      <main>
+    </section>
+    <h1>The Best Cheese Jokes</h1>
+    <div class="row">
+      <div class="col">
+        <h1> Chef Hilda</h1>
+        <img src="images/chefhilda.jpg" alt="chef Hilda" class="img-fluid">
+        <p>Some cheese may be feta than this,but this one is still quite Gouda</p>
+      </div>
+      <div class="col">
+        <h1> Chef T</h1>
+        <img src="images/chefT.jpg" alt="cheft" class="img-fluid">
+        <p>This cheese is really good if you are looking to hide livestock</p>
+      </div>
+      <div class="col">
+        <h1>Chef Tunde</H1>
+        <img src="images/cheftunde.jpg" alt="Mountains" class="img-fluid">
+        <p>At the end the night all that is left is deBrie</p>
+      </div>
+
+      <section>
+  </main>
+  <aside>
+    <article>
+      <h3>Recipe List</h3>
+      <h4>there is so much magic waiting to happen</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta optio vitae ullam tempora, debitis reiciendis corrupti nostrum voluptas porro fuga ab voluptates! Ut, vel fugiat nesciunt debitis neque quisquam corrupti.</p>
+    </article>
+    <article>
+      <h3>Create something unique</h3>
+      <h4>Select ingredients below and create something unique</h4>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius iure nihil inventore ex magnam nesciunt ipsam ea tempora exercitationem nobis! Placeat, accusantium sint. Voluptatibus magni provident natus eius debitis similique!</p>
+    </article>
+  </aside>
+  </section>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae porro fugit nesciunt enim laborum quibusdam, modi ipsa odit atque nostrum voluptatum temporibus omnis nihil sed? Minus architecto laudantium autem tempore?</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis nobis ullam officia, iste deleniti excepturi eaque? Magni modi maiores ea! Voluptatum optio aliquam vero hic amet nihil iure quidem officia?</p>
+  </Main>
+  <FOOter class="foot">
+    <p>
+      All you singles, listen up: if you’re looking to fall in love, want to start dating, ready to start a relationship, or want to keep it casual, you need to be on Tinder. With over 55 billion matches made, it’s the best free dating site to find your next best match. You’ve probably noticed; the dating landscape looks very different today, with most of us choosing to meet people online. With Tinder, the world’s most popular free dating app, you have millions of other singles at your fingertips, and they’re all ready to meet someone like you. Whether you’re straight or part of the LGBTQIA community, Tinder’s here to get the sparks flying.
+    </p>
+    <p>
+      There really is something for everyone on Tinder. Looking for a relationship? You’ve got it. Want to make friends online? Say no more. Just started uni and want to make the most of your experience? Tinder U’s got you covered. Tinder isn’t your average dating site; it’s the most diverse dating app, where adults of all backgrounds and experiences are invited to make connections, memories and everything in between.
+    </p>
+    <p>
+      Get the app!
+      <a href="#">Download for iOS</a>
+      <a href="#">Download for Android</a>
+    </p>
+    <p>
+      <a href="#">Legal</a>
+      <a href="#">Privacy</a>
+      <a href="#">Terms</a>
+      <a href="#">Cookie Policy</a>
+      <a href="#">Intellectual Property</a>
+      <a href="#">Careers</a>
+      <a href="#">Careers portal</a>
+      <a href="#">Blog</a>
+    </p>
+    <p>
+      <a href="#">Instagram</a>
+      <a href="#">TikTok</a>
+      <a href="#">YouTube</a>
+      <a href="#">Twitter</a>
+      <a href="#">Facebook</a>
+    </p>
+    <p>
+      <a href="#">FAQ</a>
+      <a href="#">Destinations</a>
+      <a href="#">Press room</a>
+      <a href="#">Contact</a>
+      <a href="#">Promo code</a>
+    </p>
+
+    <p>
+      <a href="#">FAQ</a>
+      <a href="#">Safety tips</a>
+      <a href="#">Terms</a>
+      <a href="#">Cookie Policy</a>
+      <a href="#">Privacy settings</a>
+    </p>
+    <p>
+    <h3> Creating the right connections</h3>
+    &copy; 2024 PEER PAL GROUP LLC, All Rights Reserved
+  </FOOter>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,9 @@
+<?php
+include("connect.php");
+<form action="connect.php">
+  <label for="fname">username:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="lname">password:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <input type="submit" value="Submit">
+</form>
