b7ae913830883a5263a9644531012306730c5e54
JoyRob <you@example.com>
2024-03-14 12:24:18 +0100

    NEW PROJECT

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta>
-  <img src="images/83A23CAF-8340-4ED8-81D9-4270837A3DDD.png" alt="logo" id="buisnesslogo">
+
   <title>MY AKWAIBOM KITCHEN</title>
   
   <link rel="style" href="css/style.css">
@@ -17,102 +17,114 @@
 
       <nav class="">
     
-        <ul class="navbar-nav">
-          <li class="nav-item"><a href="#" class="nav-link" >HOME</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">ABOUT</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">NEWS</a></li>
-          <li class="nav-item"><a href="#" class="nav-link">SEARCH</a></li>
-          <li class="nav-item"><a href="login.php" class="nav-link">LOGIN/REGISTER</a></li>
-        </ul>
+        <nav class="navbar navbar-expand-sm navbar-dark bg-BLACK">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="javascript:void(0)"><img src="images/83A23CAF-8340-4ED8-81D9-4270837A3DDD.png" alt="logo" id="buisnesslogo"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mynavbar">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">ABOUT</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">RECIPE LIST</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="javascript:void(0)">LOGIN/REGISTER</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="text" placeholder="Search">
+        <button class="btn btn-primary" type="button">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
       </nav>
+      <h1 class="HEADER"> WELCOME TO IBOM FOODIE</h1><br>
 
-        <!-- The video -->
-        <video autoplay muted loop id="myVideo">
-          <source src="video/Video in Dark Green White Bold Style.mp4" type="video/mp4">
-        </video>
-
-        <!-- Optional: some overlay text to describe the video -->
-        <div class="content">
-          <h1>Heading</h1>
-          <p>Lorem ipsum...</p>
-          <!-- Use a button to pause/play the video with JavaScript -->
-          <button id="myBtn" onclick="myFunction()">Pause</button>
-        </div>
-
-    </div>
-  <div >
-      <img src="images/modern-matte-black-kitchen-cabinets-obk22-dark-1024.webp" alt="logo" id="mainimgage">
-    </div>
-    <h1 class="HEADER"> WELCOME TO IBOM FOODIE</h1>
+  <div class="text-center">
+      <img src="images/modern-matte-black-kitchen-cabinets-obk22-dark-1024.webp" >
+    </div><br>
 
   
   </HEAder>
   <Main class="main">
     <section>
-      <h2>Meet the CEO</h2>
-      <img src="images/IMG_1116.JPG" alt="ceo" id="joy">
-
+    
       <main>
-        <section>
-          <h1> SOME OF OUR FAVORITE RECIPES</h1>
+        
+        <SECtion><h2 class="text-center"> LEARN THE BEST AKWA IBOM RECIPES FROM THE BEST CHEFS IN NIGERIA</h2>
+          <br>
           <div class="container text-center">
             <div class="row align-items-start">
               <div class="col">
-                <h1> AFANG SOUP</h1>
+                <h3> CHEF T</h3>
+                <img src="images/chefT.jpg" alt="chef Hilda" class="img-fluid ">
+                <p>Some cheese may be feta than this,but this one is still quite Gouda</p>
+              </div> 
+              <div class="col">
+                <h3>CHEF HILDA</H3>
+                <img src="images/chefhilda.jpg" alt="Mountains"class="img-fluid">
+                <p>At the end the night all that is left is deBrie</p>
+              </div>
+              <div class="col">
+                <h3> CHEF TUNDE</h3>
+                <img src="images/cheftunde.jpg" alt="cheft"class="img-fluid">
+                <p>This cheese is really good if you are looking to hide livestock</p>
+              </div>
+            </SECtion>
+        <section><br>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora voluptatem dolorem omnis at voluptatibus a provident sapiente natus, est incidunt explicabo, architecto voluptates ducimus cum aliquam illum. Inventore, ullam omnis?</p>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ullam nemo ipsa, velit reiciendis id blanditiis commodi repudiandae aliquam quidem, minus in aliquid voluptates dolores ab reprehenderit rem quos neque.</p>
+      <h2 class="text-center"> SOME OF OUR BEST SELLING RECIPES</h2>
+          <br>
+          <div class="container text-center">
+            <div class="row align-items-start">
+              <div class="col">
+                <h3> AFANG SOUP</h3>
                 <img src="images/afang.jpg" alt="chef Hilda" class="img-fluid ">
                 <p>Some cheese may be feta than this,but this one is still quite Gouda</p>
               </div>
               <div class="col">
-                <h1> JOLLOF RICE</h1>
+                <h3> JOLLOF RICE</h3>
                 <img src="images/jollof.jpeg" alt="cheft"class="img-fluid">
                 <p>This cheese is really good if you are looking to hide livestock</p>
               </div>
               <div class="col">
-                <h1>EDIKA EKONG</H1>
+                <h3>EDIKA EKONG</H3>
                 <img src="images/EDIKAEKONG.jpg" alt="Mountains"class="img-fluid">
                 <p>At the end the night all that is left is deBrie</p>
               </div>
         </section>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, iste ratione vitae iusto dolor exercitationem alias sequi sed ad saepe deserunt, corrupti quam. Explicabo voluptas ut non quis officia alias.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste repellendus delectus autem nulla vel eligendi deleniti et aut accusantium blanditiis. Minus enim perspiciatis quas saepe, repellendus a rem iste magnam!</p>
+        <br>
+          <iframe title="vimeo-player" src="https://player.vimeo.com/video/923134324?h=232b407a33" width="100" height="360" frameborder="0"   autoplay allowfullscreen class="container text-center"></iframe>
       </main>
 
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi neque ipsam soluta omnis, animi, voluptate sunt cumque perferendis nesciunt rem, corporis dolorem nemo in maxime facilis cupiditate ullam? Tempora, amet?</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur reprehenderit libero velit quibusdam minus, dolorem laborum inventore ea distinctio eos amet eum sunt dolore saepe quae in sed a exercitationem.</p>
-      <h3>This is where the magic happens</h3>
+      <h3 class="container text-center">This is where the magic happens</h3>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus aliquam odit, corrupti aperiam impedit obcaecati voluptatem quia beatae, unde sapiente quo nobis numquam blanditiis modi quisquam deleniti cum! Quas, aliquid.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio quisquam harum, laudantium officia, necessitatibus unde facilis nisi assumenda, distinctio debitis minima tenetur corrupti minus voluptatibus commodi velit officiis neque. Incidunt?</p>
-      <h3>Create your own recipes from our list of comprehensive ingredients</h3>
+      <h3 class="container text-center">Create your own recipes from our list of comprehensive ingredients</h3>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis ullam in ipsam culpa dolorum ea! Nobis, dolorem. Error deleniti repellendus provident ipsam, recusandae doloribus exercitationem voluptas eum distinctio omnis cumque?</p>
-      <main>
-    </section>
-    <h1>The Best Cheese Jokes</h1>
-    <div class="row">
-      <div class="col">
-        <h1> Chef Hilda</h1>
-        <img src="images/chefhilda.jpg" alt="chef Hilda" class="img-fluid">
-        <p>Some cheese may be feta than this,but this one is still quite Gouda</p>
-      </div>
-      <div class="col">
-        <h1> Chef T</h1>
-        <img src="images/chefT.jpg" alt="cheft" class="img-fluid">
-        <p>This cheese is really good if you are looking to hide livestock</p>
-      </div>
-      <div class="col">
-        <h1>Chef Tunde</H1>
-        <img src="images/cheftunde.jpg" alt="Mountains" class="img-fluid">
-        <p>At the end the night all that is left is deBrie</p>
-      </div>
-
-      <section>
-  </main>
+      
   <aside>
     <article>
-      <h3>Recipe List</h3>
-      <h4>there is so much magic waiting to happen</h4>
+      <h3 class="container text-center">Recipe List</h3>
+      <h4 class="container text-center">there is so much magic waiting to happen</h4>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta optio vitae ullam tempora, debitis reiciendis corrupti nostrum voluptas porro fuga ab voluptates! Ut, vel fugiat nesciunt debitis neque quisquam corrupti.</p>
     </article>
     <article>
-      <h3>Create something unique</h3>
-      <h4>Select ingredients below and create something unique</h4>
+      <h3 class="container text-center">Create something unique</h3>
+      <h4 class="container text-center">Select ingredients below and create something unique</h4>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius iure nihil inventore ex magnam nesciunt ipsam ea tempora exercitationem nobis! Placeat, accusantium sint. Voluptatibus magni provident natus eius debitis similique!</p>
     </article>
   </aside>
@@ -121,17 +133,8 @@
   <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis nobis ullam officia, iste deleniti excepturi eaque? Magni modi maiores ea! Voluptatum optio aliquam vero hic amet nihil iure quidem officia?</p>
   </Main>
   <FOOter class="foot">
-    <p>
-      All you singles, listen up: if you’re looking to fall in love, want to start dating, ready to start a relationship, or want to keep it casual, you need to be on Tinder. With over 55 billion matches made, it’s the best free dating site to find your next best match. You’ve probably noticed; the dating landscape looks very different today, with most of us choosing to meet people online. With Tinder, the world’s most popular free dating app, you have millions of other singles at your fingertips, and they’re all ready to meet someone like you. Whether you’re straight or part of the LGBTQIA community, Tinder’s here to get the sparks flying.
-    </p>
-    <p>
-      There really is something for everyone on Tinder. Looking for a relationship? You’ve got it. Want to make friends online? Say no more. Just started uni and want to make the most of your experience? Tinder U’s got you covered. Tinder isn’t your average dating site; it’s the most diverse dating app, where adults of all backgrounds and experiences are invited to make connections, memories and everything in between.
-    </p>
-    <p>
-      Get the app!
-      <a href="#">Download for iOS</a>
-      <a href="#">Download for Android</a>
-    </p>
+
+  
     <p>
       <a href="#">Legal</a>
       <a href="#">Privacy</a>
@@ -149,13 +152,7 @@
       <a href="#">Twitter</a>
       <a href="#">Facebook</a>
     </p>
-    <p>
-      <a href="#">FAQ</a>
-      <a href="#">Destinations</a>
-      <a href="#">Press room</a>
-      <a href="#">Contact</a>
-      <a href="#">Promo code</a>
-    </p>
+    
 
     <p>
       <a href="#">FAQ</a>
@@ -164,9 +161,9 @@
       <a href="#">Cookie Policy</a>
       <a href="#">Privacy settings</a>
     </p>
-    <p>
+   
     <h3> Creating the right connections</h3>
-    &copy; 2024 PEER PAL GROUP LLC, All Rights Reserved
+    &copy; 2024 IBOM F00DIE GROUP LLC, All Rights Reserved
   </FOOter>
 </body>
 
